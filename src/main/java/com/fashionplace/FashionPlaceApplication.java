859d18109be31ece64bb6ca92b2b7f71c8b9d762
Noa Haberer <noahab@std.hac.ac.il>
2026-06-09 11:26:35 +0300

    initial commit

---
+++ src/main/java/com/fashionplace/FashionPlaceApplication.java
@@ -0,0 +1,11 @@
+package com.fashionplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionPlaceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FashionPlaceApplication.class, args);
+    }
+}
